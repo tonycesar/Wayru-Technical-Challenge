654eef15948a6e0aa2f642aa7efcad348acdf36d
César Calle <mcalle@ulasalle.edu.pe>
2022-08-03 00:07:24 -0500

    feat: add contract functions

---
+++ solidity/contracts/WayruPool.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WayruPool {
+
+  constructor(){
+  }
+
+  function sendAmount(uint _data) external  {
+  }
+
+  function withdraw() external {
+    
+  }
+
+  function transferOwnerTo(address owner) external {
+
+  }
+
+  function isOwner() external view returns(bool) {
+    return false;
+  }
+  
+}
